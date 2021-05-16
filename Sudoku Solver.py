c2c28e9d68e868bc463614ec6e55256282f3aeb5
0980991 <0980991@hr.nl>
2021-05-16 13:08:32 +0200

    Some UI stuff

--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -7,28 +7,75 @@ import random
 import numpy as np
 
 
-class sudokuSquare:
-    sudoku_square = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+class Sudoku3x3:
+    def __init__(self, coordinate):
+        if coordinate == "a":
 
-class sudokuGrid:
-    def getNewGrid(self):
-        sudoku_square = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.sudoku3x3 = [[1.1, 1.2, 1.3], [2.1, 2.2, 2.3], [3.1, 3.2, 3.3]]
 
-        sudoku_grid   = [sudoku_square, sudoku_square, sudoku_square]
-        sudoku_size   = [sudoku_grid,   sudoku_grid,   sudoku_grid  ]
-        for bigrow in sudoku_size:
-            for row in sudoku_grid:
+    def Row(self, colNr):
+        rowString = []
+        for index in range(0, 3):
+            
+            rowString.append(str(self.sudoku3x3[colNr][index]))
+            rowString.append(" | ")
+            if index == 2 and colNr != 2:
+                rowString.append(" | ")
+            
+        
+        return (''.join(rowString))
+    
+    def PrintSquare(self):
+        squareString = []
+        for index in range(0, 3):
+            squareString.append("-------------  -------------  -------------\n| ")
+            for rowNr in range(0, 3):
+                
+                squareString.append(self.Row(rowNr))
+            squareString.append("\n")
+        squareString.append("-------------  -------------  -------------")
+        print(''.join(squareString))
+    
+    
+
+
+class Sudoku9x9:
+    def __init__(self):
+        allValues = np.matrix([[0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0],
+                     [0,0,0,0,0,0,0,0]])
+        
+        #self.sudoku9x9 = [Sudoku3x3(), Sudoku3x3(), Sudoku3x3()]
+        
+
+    def PrintGrid(self):
+        string3x3 = []
+        for index in range(0, 3):
+            self.sudoku9x9[index].PrintSquare()
+'''
+        for bigrow in sudokuSize:
+            for row in sudoku9x9:
                 for elem in row:
                      
-                    print(elem, end=' || ')
+                    print(elem, end=' | ')
                     
                 if len(elem) % 3 == 0: 
                     print("\n")    
-            print("======================================")
-    #def getSudoku(self.sudoku_grid):
+            print("----------------------------------")
+'''
+    #def getSudoku(self.sudoku9x9):
 
 
-sudokuGrid.getNewGrid()
+#Sudoku9x9.GetNegetNewGrid()
+
+grid = Sudoku9x9()
+grid.PrintGrid()
 
     
         
